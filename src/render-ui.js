11f0bb62c62a7f47906581142de8bef7c4e2530f
weizhixie <wx02697@yahoo.co.uk>
2025-10-08 17:17:51 +0100

    Add task form and modal for adding new task

--- src/render-ui.js
+++ src/render-ui.js
@@ -21,10 +21,111 @@ export class renderUI {
 
         const todosContainer = document.createElement("div");
         todosContainer.classList.add("todos-container");
-        todosContainer.textContent = "Todos container";
 
+        const addTaskBtn = document.createElement("button");
+        addTaskBtn.classList.add("add-task-btn");
+        addTaskBtn.textContent = `\u2295 Add Task`;
+        addTaskBtn.addEventListener("click", () => this.openAddTaskModal());
+
+        todosContainer.appendChild(addTaskBtn);
         mainSection.append(sideBar, todosContainer);
+
         return mainSection;
+    }
+
+    openAddTaskModal() {
+        let addTaskModal = document.querySelector(".add-task-modal");
+
+        if (!addTaskModal) {
+            addTaskModal = document.createElement("dialog");
+            addTaskModal.classList.add("add-task-modal");
+
+            const xClosetBtn = document.createElement("button");
+            xClosetBtn.classList.add("x-closet-btn");
+            xClosetBtn.textContent = '\u2715';
+            xClosetBtn.addEventListener("click", () => addTaskModal.close());
+
+            addTaskModal.append(this.createTaskForm(), xClosetBtn);
+            this.body.appendChild(addTaskModal);
+        }
+
+        addTaskModal.showModal();
+    }
+
+    createTaskForm() {
+        const taskForm = document.createElement("form");
+        taskForm.classList.add("task-form");
+
+        const taskFormHeader = document.createElement("h2");
+        taskFormHeader.classList.add("task-form-header");
+        taskFormHeader.textContent = "Add Task";
+        taskForm.appendChild(taskFormHeader);
+
+        const taskFormFields = [{
+            label: "Title*",
+            input: { tagName: "input", type: "text", name: "todo-title", id: "todo-title", required: true }
+        }, {
+            label: "Description",
+            input: { tagName: "textarea", name: "todo-description", id: "todo-description", rows: 5 }
+        }, {
+            label: "Due Date",
+            input: { tagName: "input", type: "date", name: "todo-dueDate", id: "todo-dueDate" }
+        }, {
+            label: "Priority",
+            input: { tagName: "select", name: "priority-select", id: "priority-select" },
+            options: [
+                { value: "low", text: "Low" },
+                { value: "normal", text: "Normal" },
+                { value: "high", text: "High" }]
+        },];
+
+        taskFormFields.forEach((field) => {
+            const formWrapper = document.createElement("div");
+            formWrapper.classList.add("task-form-wrapper");
+
+            const label = document.createElement("label");
+            label.htmlFor = field.input.id;
+            label.textContent = field.label;
+
+            const input = document.createElement(field.input.tagName);
+            input.classList.add("task-input");
+            for (const key in field.input) {
+                if (key !== "tagName") {
+                    input.setAttribute(key, field.input[key]);
+                }
+            }
+
+            if (field.input.tagName === "select" && field.options) {
+                field.options.forEach(optData => {
+                    const option = document.createElement("option");
+                    option.value = optData.value;
+                    option.textContent = optData.text;
+                    input.appendChild(option);
+                });
+            }
+            formWrapper.append(label, input);
+            taskForm.appendChild(formWrapper);
+        });
+
+        const taskFormBtnWrapper = document.createElement("div");
+        taskFormBtnWrapper.classList.add("task-form-wrapper", "task-form-btn-wrapper");
+
+        const cancelBtn = document.createElement("button");
+        cancelBtn.classList.add("cancel-btn");
+        cancelBtn.textContent = "Cancel";
+        cancelBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            this.body.querySelector(".add-task-modal").close();
+        });
+
+        const submitBtn = document.createElement("button");
+        submitBtn.classList.add("submit-task-btn");
+        submitBtn.textContent = "Add Task";
+
+        taskFormBtnWrapper.append(cancelBtn, submitBtn);
+        taskForm.appendChild(taskFormBtnWrapper);
+
+        return taskForm;
     }
 
     renderTasks(tasks) {
